f39e3ebec5bf9b020cdcab19bdf85484c9a4caf1
Laurilun <lauri.lundell@gmail.com>
2019-01-12 23:43:36 +0200

    now it works

---
+++ javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	
+	
+	
+	$(".logo").on("click", function(){
+    	$(".logo").toggleClass("turned-off");
+			$(".contact-button").toggle(100);
+			$(".intro").toggle(100);
+			$(".speech-bubble").toggle(300);
+    
+  });
+	
+	
+		$(".email-link").on("click", function(){
+    	$(".email-address").toggleClass("invisible text-trans");
+  });
+	
+	
+		$(".work-link").on("click", function(){
+			$('html, body').animate({
+					scrollTop: $("#work-section").offset().top -50
+			}, 500);
+	});
+
+			$(".contact-link, .contact-button").on("click", function(){
+				$('html, body').animate({
+						scrollTop: $("#contact-form").offset().top-50
+				}, 500);
+	});
+
+	
+	
+});
